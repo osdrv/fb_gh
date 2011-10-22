4864d2387194bfd97f6fffd1fee9b8364d62b2a1
4pcbr <o.s.sidorov@gmail.com>
2011-10-22 20:05:19 +0400

    создал приложение

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "http://rubygems.org"
+
+gem "rails", "3.1.1"
+gem "haml", "~> 3.1.3"
+gem "jquery-rails"
+
+gem "mongoid", "~> 2.3.2"
+gem "bson_ext", "~> 1.4.1"
+
+gem "octopi", "~> 0.4.5"
+
+group :assets do
+  gem "sass-rails",   "~> 3.1.4"
+  gem "coffee-rails", "~> 3.1.1"
+  gem "uglifier", ">= 1.0.3"
+end
+
+gem "unicorn"
+gem "heroku", "~> 2.11.0"
+
+group :test do
+  gem "rspec-rails", "~> 2.7.0"
+  gem "factory_girl", "~> 2.2.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbGh::Application.config.secret_token = '1a92656e4002820500c25692e612d1cfcb61ac5031ce217fe46bf42a5e8c449f239f6249d5012b60916a41e9a6fb5a27204d4aaacd23c6c92593aca950705bfd'
