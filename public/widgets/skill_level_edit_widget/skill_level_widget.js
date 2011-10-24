51683e763e6818b27d0eb50c9308c4ab658ab496
unknown <andr@andr_pc.(none)>
2011-10-24 11:16:09 +0400

    skill edit widget v0.2. added options parameter. fixed tags attributes. bar-clicked enabled

--- public/widgets/skill_level_edit_widget/skill_level_widget.js
+++ public/widgets/skill_level_edit_widget/skill_level_widget.js
@@ -1,30 +1,56 @@
-function initWidget(input, min, max, value, valueChangedCallBack) {
-    value = value || 0
-    valueChangedCallBack = valueChangedCallBack || function() {}
-    min = min || 0
-    max = max || 10
-    var widget_html = "<div id='gui-input'><span id='name-span' class='float-left'></span><div id='decrease-level-button' class='level-button float-left'></div><div class='float-left' id='level-container'><div id='level-1' class='level-item float-left' ></div><div id='level-2' class='level-item float-left'></div><div id='level-3' class='level-item float-left'></div><div id='level-4' class='level-item float-left'></div><div id='level-5' class='level-item float-left'></div><div id='level-6' class='level-item float-left'></div><div id='level-7' class='level-item float-left'></div><div id='level-8' class='level-item float-left'></div><div id='level-9' class='level-item float-left'></div><div id='level-10' class='level-item float-left'></div></div><div class='level-button float-left' id='increase-level-button'></div></div>"
+widget_html = " <div class='gui-input'> <span class='name-span float-left'></span> <div class='decrease-level-button level-button float-left'></div> <div class='float-left level-container'> </div> <div class='increase-level-button level-button float-left'></div></div>"
+bar_html = "<div class='level-item float-left' ></div>"
+
+function propertyDefaultGetter(hash, property, defValue) {
+    if (hash.hasOwnProperty(property))
+        return hash[property]
+    else
+        return defValue
+}
+
+function initSkillLevelWidget(input, options) {
+    var value = propertyDefaultGetter(options, 'value', 0)
+    var onBeforeValueChanged = propertyDefaultGetter(options, 'onBeforeValueChanged', function(){})
+    var onAfterValueChanged = propertyDefaultGetter(options, 'onAfterValueChanged', function(){})
+    var min = 1
+    var max = propertyDefaultGetter(options, 'max', 10)
     var skill_name = $(input).attr('name')
     $(input).wrap('<div id=skill-widget-' + skill_name + '></div>').after(widget_html)
     $(input).val(value)
-    var div_selector = '#skill-widget-' + skill_name
-    $(div_selector + ' #name-span').text(skill_name)
-    $(div_selector + ' .level-item:lt(' + value + ')').each(function() {
-        $(this).css('background-color', 'green')
-    })
-    $(div_selector + ' #decrease-level-button').click(function() {
-        v = parseInt($(input).val())
-        console.log(v, v-1, min)
+    var item_div_id = '#skill-widget-' + skill_name
+    $(item_div_id + ' .name-span').text(skill_name)
+    for (var i = min; i <= max; i++) {
+        $(item_div_id + ' .level-container').before("<div class='level-item float-left' value=" + i + "></div>")
+        if (i <= value) {
+            $(item_div_id + ' .level-item[value=' + i + ']').addClass('level-item-achieved')
+        }
+    }
+    $(item_div_id + ' .decrease-level-button').click(function() {
+        var v = parseInt($(input).val())
         if (v - 1 >= min) {
+            onBeforeValueChanged()
             $(input).val(v - 1)
-            $(div_selector + ' #level-' + v).css('background-color', 'white')
+            onAfterValueChanged()
+            $(item_div_id + ' .level-item[value=' + v + ']').removeClass('level-item-achieved')
         }
     })
-    $(div_selector + ' #increase-level-button').click(function() {
-        v = parseInt($(input).val())
+    $(item_div_id + ' .increase-level-button').click(function() {
+        var v = parseInt($(input).val())
         if (v + 1 <= max) {
+            onBeforeValueChanged()
             $(input).val(v + 1)
-            $(div_selector + ' #level-' + (v+1)).css('background-color', 'green')
+            onAfterValueChanged()
+            $(item_div_id + ' .level-item[value=' + (v + 1) + ']').addClass('level-item-achieved')
         }
+    })
+    $(item_div_id + ' .level-item').click(function() {
+        var i, v = parseInt($(this).attr('value'));
+        for (i = min-1; i <= v; i++)
+            $(item_div_id + ' .level-item[value=' + i + ']').addClass('level-item-achieved')
+        for (i = v+1; i <= max; i++)
+            $(item_div_id + ' .level-item[value=' + i + ']').removeClass('level-item-achieved')
+        onBeforeValueChanged()
+        $(input).val($(this).attr('value'))
+        onAfterValueChanged()
     })
 }
